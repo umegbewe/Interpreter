8fcb29a7359ed6da70563a8ea66b1b7cb6806314
Umegbewe Nwebedu <nwebedujunior55@gmail.com>
2021-08-21 20:27:32 +0000

    if argument is a letter

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -7,6 +7,14 @@ func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
 	switch l.ch {
+
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
 	case ';':
@@ -53,6 +61,18 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
